33216532c4e1570905ad2c850f10f218c80cb7ff
anargyaa <43247002+anargyaa@users.noreply.github.com>
2023-06-14 06:14:15 +0700

    Delete pwku directory

--- pwku/controller/filter.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conn.php';
-if(isset($_GET['tanggal'])){
-	$tgl = $_GET['tanggal'];
-	$sql = mysqli_query($conn,"select * from keluar where tglKeluar='$tgl'");
-	$query = mysqli_query($conn, "SELECT * FROM keluar INNER JOIN barang ON keluar.idBarang = barang.idBarang INNER JOIN merk ON keluar.idMerk = merk.idMerk WHERE tglKeluar='$tgl'");
-	echo "<script>alert('Data berhasil dirubah');window.location='../index.php?halaman=laporan';</script>";
-}
-?>
--- pwku/controller/hapus-proc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$ambil = $conn->query("SELECT * FROM barang WHERE idBarang = '$_GET[id]'");
-$pecah = $ambil->fetch_assoc();
-
-$conn->query("DELETE FROM barang WHERE idBarang = '$_GET[id]'");
-
-echo "<script>alert('Data berhasil dihapus');</script>";
-echo "<script>location='./index.php';</script>";
